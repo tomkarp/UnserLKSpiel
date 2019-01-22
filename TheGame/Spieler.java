e23eaf22e76f3aa0487f1bdefc9b5ab0a0c186d9
Till Merdian <t.merdian@t-online.de>
2019-01-22 14:46:59 +0100

    Neue Bilder für Spieler

--- TheGame/Spieler.java
+++ TheGame/Spieler.java
@@ -2,7 +2,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Spieler extends Actor implements Treffbar
 {
-<<<<<<< HEAD
     int leben;
     int i;
     int x;
@@ -39,6 +38,7 @@ public class Spieler extends Actor implements Treffbar
     } 
 
     public void checkLeben(){
+        getWorld().showText("Leben: "+leben,100,100);
         if(leben<=0){
             getWorld().showText("GAME OVER",getWorld().getWidth()/2,getWorld().getHeight()/2);
             Greenfoot.stop();
@@ -133,127 +133,4 @@ public class Spieler extends Actor implements Treffbar
             setImage("CharakterR.png");
         }
     }
-=======
-	int leben;
-	int i;
-	int x;
-	int y;
-	double blickWinkel;
-
-	double alpha;
-	double beta;
-	double a2 ;
-	double b2; 
-	Waffen aktuelleWaffe; 
-
-	public Spieler(){
-		blickWinkel = 1;
-		i = 1;
-		leben=10000;
-	}
-
-	public void act() 
-	{
-	    checkLeben();
-		if (i> 0){
-			waffenWechsel();
-			i--;
-		}
-		x = getX();
-		y = getY();
-		bewegen();
-		if(aktuelleWaffe !=  null){
-			kreis();
-			attacke();
-		}
-	} 
-	public void checkLeben(){
-	    getWorld().showText("Leben: "+leben,100,100);
-	    if(leben<=0){
-	        getWorld().showText("GAME OVER",getWorld().getWidth()/2,getWorld().getHeight()/2);
-	        Greenfoot.stop();
-	       }
-	   }
-
-	public void damage(int s){
-		leben = leben - s;
-	}
-
-	private void kreis(){
-		if(Greenfoot.getMouseInfo() != null){
-			int a = Greenfoot.getMouseInfo().getX() - getX();
-			int b = Greenfoot.getMouseInfo().getY() - getY();
-			double c = Math.sqrt(a*a +  b*b);
-			final double gamma = 90;
-			final int c2 = 40;
-			if(a >= 0 && b > 0){
-				beta = Math.toDegrees(Math.asin(b / c));
-				blickWinkel = beta + 90;
-				alpha = 180 - beta - gamma;
-
-				a2 = Math.sin(alpha * Math.PI / 180) * c2;
-				b2 = Math.sin(beta * Math.PI / 180) * c2;   
-
-				aktuelleWaffe.tragen(getX() + (int)a2 , getY() + (int)b2 , (int)blickWinkel);
-			}
-			if(a < 0 && b >= 0){
-				a = a * -1;
-				beta = Math.toDegrees(Math.asin(b / c));
-				blickWinkel = (beta - 270) * -1 ;
-				alpha = 180 - beta - gamma;
-
-				a2 = Math.sin(alpha * Math.PI / 180) * c2;
-				b2 = Math.sin(beta * Math.PI / 180) * c2;   
-
-				aktuelleWaffe.tragen(getX() - (int)a2 , getY() + (int)b2 , (int)blickWinkel);
-			}
-			if(a <= 0 && b < 0){
-				a = a * -1;
-				b = b * -1;
-				beta = Math.toDegrees(Math.asin(b / c));
-				blickWinkel = beta + 270;
-				alpha = 180 - beta - gamma;
-
-				a2 = Math.sin(alpha * Math.PI / 180) * c2;
-				b2 = Math.sin(beta * Math.PI / 180) * c2;   
-
-				aktuelleWaffe.tragen(getX() - (int)a2 , getY() - (int)b2 , (int)blickWinkel);
-			}
-			if(a > 0 && b <= 0){
-				b = b * -1;
-				beta = Math.toDegrees(Math.asin(b / c));
-				blickWinkel = (beta - 90) * -1 ;
-				alpha = 180 - beta - gamma;
-
-				a2 = Math.sin(alpha * Math.PI / 180) * c2;
-				b2 = Math.sin(beta * Math.PI / 180) * c2;   
-
-				aktuelleWaffe.tragen(getX() + (int)a2 , getY() - (int)b2 , (int)blickWinkel);
-			}
-		}
-	}
-
-	public void attacke(){
-		if(Greenfoot.isKeyDown("space")){
-			aktuelleWaffe.attacke((int)blickWinkel);
-		}
-	}
-
-	public void waffenWechsel(){
-		Doomsday p = new Doomsday();
-		aktuelleWaffe = p;
-		getWorld().addObject(p ,getX() ,getY() );
-	}
-
-	private void bewegen(){
-		if(Greenfoot.isKeyDown("w"))
-			setLocation(getX(), getY() - 3);
-		if(Greenfoot.isKeyDown("s"))
-			setLocation(getX(), getY() + 3);
-		if(Greenfoot.isKeyDown("a"))
-			setLocation(getX() - 3, getY());
-		if(Greenfoot.isKeyDown("d"))
-			setLocation(getX() + 3, getY());
-	}
->>>>>>> cad0b6fc2f86fe33b2dec7eff2c56d834e5ec952
 }
